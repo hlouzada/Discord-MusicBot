f9b3bbabb233a546737cca81724d44c973e0ff9c
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2024-12-11 23:45:11 -0300

    fix: timeout subcommand

--- djs-bot/commands/utility/config/timeout.js
+++ djs-bot/commands/utility/config/timeout.js
@@ -1,69 +1,90 @@
-const { config } = require("dotenv");
 const { setReplyDeleteTimeout } = require("../../../util/message");
 const { setAutoLeaveTimeout } = require("../../../util/musicManager");
-
 
 /**
  * @param {import("../../../lib/SlashCommand")} baseCommand
  */
 module.exports = function timeout(baseCommand) {
-	baseCommand.addSubcommand((command) =>
-		command
-		.setName("timeout")
-		.setDescription("Set server timeout")
-		.addStringOption((opt) =>
-			opt
-			.setName("option")
-			.setDescription("Select reply or disconnect timeout")
-			.setRequired(true)
-			.addChoices(
-				{ name: "Reply", value: "reply" },
-				{ name: "Disconnect", value: "disconnect" },				
-			)
-		)
-		.addIntegerOption((opt) =>
-			opt
-			.setName("timeout")
-			.setDescription("Set timeout in seconds, leave empty to reset")
-		)
-	);
+	baseCommand
+		.addSubcommandGroup((group) =>
+			group
+				.setName("timeout")
+				.setDescription("Set server timeout settings")
+				.addSubcommand((command) =>
+					command
+						.setName("reply")
+						.setDescription("Set reply timeout")
+						.addIntegerOption((opt) =>
+							opt
+								.setName("timeout")
+								.setDescription("Set timeout in seconds, leave empty to reset")
+						)
+				)
+				.addSubcommand((command) =>
+					command
+						.setName("disconnect")
+						.setDescription("Set disconnect timeout")
+						.addIntegerOption((opt) =>
+							opt
+								.setName("timeout")
+								.setDescription("Set timeout in seconds, leave empty to reset")
+						)
+				)
+		);
 
-	return baseCommand.setSubCommandHandler(
-		"timeout",
-		async function (client, interaction, options) {
-			const guildId = interaction.guild.id;
+	baseCommand.setSubCommandHandler("timeout reply", async function (client, interaction, options) {
+		const guildId = interaction.guild.id;
+		let timeout = (options.getInteger("timeout") || 0) * 1000;
 
-			let timeout = (options.getInteger("timeout") || 0) * 1000;
-
-			if (timeout && timeout < 1) {
-				return interaction.reply({
-					embeds: [
-						client.redEmbed({
-							desc: "Timeout must be greater than 0",
-						}),
-					],
-					ephemeral: true,
-				});
-			}
-
-			const is_reply = options.getString("option") === "reply";
-
-			if (is_reply) {
-				timeout = timeout || config.defaultConfig.replyDeleteTimeout;
-				setReplyDeleteTimeout(guildId, timeout);
-			} else {
-				timeout = timeout || config.defaultConfig.autoLeaveTimeout;
-				setAutoLeaveTimeout(guildId, timeout);
-			}
-
+		if (timeout && timeout < 1) {
 			return interaction.reply({
 				embeds: [
-					client.greenEmbed({
-						desc: `${is_reply ? "Reply" : "Disconnect"} timeout set to ${timeout/1000} seconds`,
+					client.redEmbed({
+						desc: "Timeout must be greater than 0",
 					}),
 				],
+				ephemeral: true,
 			});
-
 		}
-	);
+
+		timeout = timeout || client.config.defaultConfig.replyDeleteTimeout;
+		setReplyDeleteTimeout(guildId, timeout);
+
+		return interaction.reply({
+			embeds: [
+				client.greenEmbed({
+					desc: `Reply timeout set to ${timeout / 1000} seconds`,
+				}),
+			],
+		});
+	});
+
+	baseCommand.setSubCommandHandler("timeout disconnect", async function (client, interaction, options) {
+		const guildId = interaction.guild.id;
+		let timeout = (options.getInteger("timeout") || 0) * 1000;
+
+		if (timeout && timeout < 1) {
+			return interaction.reply({
+				embeds: [
+					client.redEmbed({
+						desc: "Timeout must be greater than 0",
+					}),
+				],
+				ephemeral: true,
+			});
+		}
+
+		timeout = timeout || client.config.defaultConfig.autoLeaveTimeout;
+		setAutoLeaveTimeout(guildId, timeout);
+
+		return interaction.reply({
+			embeds: [
+				client.greenEmbed({
+					desc: `Disconnect timeout set to ${timeout / 1000} seconds`,
+				}),
+			],
+		});
+	});
+
+	return baseCommand;
 };
